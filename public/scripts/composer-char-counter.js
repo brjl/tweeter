eaf0e305e58e3a1b496ba9c76f2850e595c3e27b
Liam Butler <lrbutler@gmail.com>
2021-02-24 01:33:57 +0000

    Add fixed nav bar

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,32 +1,29 @@
 $(document).ready(function () {
-  // --- our code goes here ---
   $("#tweet-text").keyup(onKeyUp);
 });
 
 const onKeyUp = function (event) {
-  //console.log(event.target)
   const parent = $(this).parent();
-  //console.log(parent)
   const output = parent.find("output");
   const button = parent.find("button");
   let charactersTyped = $(this).val().length;
   let charactersLeft = 140 - charactersTyped;
   console.log(button);
 
-  //$(".counter").html(charactersLeft)
+  //$(".counter").html(charactersLeft) //direct way to access
   output.html(charactersLeft);
   button.prop("disabled", false);
 
   if (charactersLeft < 0) {
-    //disable tweet button and turn charcount red
-
-    output.css({ color: "red" });
-    //button.disabled = true;
+    //output.css({ color: "red" });
+    output.addClass("counter-red"); //not working, but what I need to use
     button.prop("disabled", true);
     button.fadeTo(0, 0.25);
   }
 
   if (charactersLeft > 0) {
+    //output.css({ color: #545149 });
+    output.removeClass("counter-red"); //not working, but what I need to use
     button.fadeTo(0, 1);
   }
 };
